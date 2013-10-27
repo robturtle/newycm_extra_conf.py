9b3b21e8e07bd78aa744c09ba8dd27a5d29f232a
LIU Yang <JeremyRobturtle@gmail.com>
2013-10-28 05:37:54 +0800

    Fix config ycm.c.py

--- ycm.c.py
+++ ycm.c.py
@@ -55,7 +55,6 @@ flags = [
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
 'c',
 '-isystem',
 '../BoostParts',
