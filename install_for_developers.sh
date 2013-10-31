e2b45f280a1e48ac85a182455253e3cfa5fbfff7
LIU Yang <JeremyRobturtle@gmail.com>
2013-10-31 13:11:49 +0800

    A very simple but time-saving install script.

---
+++ install_for_developers.sh
@@ -0,0 +1,17 @@
+# Filename: install_for_developers.sh
+# Author:   LIU Yang
+# Create Time: Thu Oct 31 13:03:20 HKT 2013
+# License:     LGPL v2.0+
+# Contact Me:  JeremyRobturtle@gmail.com
+# Brief: Just make symlinks
+
+# If you didn't add ~/bin to your PATH
+# I suppose you didn't changed default shell either :-)
+test -d ~/bin || echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+test -d ~/bin || mkdir ~/bin
+test -d ~/Templates || mkdir ~/Templates
+
+ln -s $(pwd)/newycm_extra_conf.py ~/bin
+for template in $(ls ycm.*.py); do
+    ln -s "$template" ~/Templates
+done
