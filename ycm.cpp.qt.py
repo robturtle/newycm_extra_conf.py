702f6d891af3a3c7c743ab53dc67dd9303a5b12e
LIU Yang <JeremyRobturtle@gmail.com>
2014-04-03 20:40:52 +0800

    Template qt inject flags using pkg-config on OS X

--- ycm.cpp.qt.py
+++ ycm.cpp.qt.py
@@ -1,13 +1,18 @@
+# NOTE Things I marked as TODO is not nessessary means I
+#      will do it myself someday. It could be a suggestion
+#      for anyone interested in it.
+
+# NOTE for Linux user
 # Qt common flags
+# The Qt pkg-config module in Gentoo is Qt4/Qt5, But in OS X it's splited into many sub-modules.
+# Do the compatible work yourself if needed
+QtModules = ["QtCLucene", "QtDeclarative", "QtDesignerComponents", "QtHelp", "QtNetwork", "QtScript", "QtSql", "QtTest", "QtUiTools_debug", "QtXml",
+             "QtCore", "QtDesigner", "QtGui", "QtMultimedia", "QtOpenGL", "QtScriptTools", "QtSvg", "QtUiTools", "QtWebKit", "QtXmlPatterns"]
 
+from subprocess import Popen
+from subprocess import PIPE
+qtflags = []
+for module in QtModules:
+    qtflags += Popen(['pkg-config', '--cflags'] + QtModules, stdout=PIPE).communicate()[0].split()
 
-flags += [
-'-DQT_CORE_LIB',
-'-DQT_GUI_LIB',
-'-DQT_NO_DEBUG',
-'-I/usr/include/qt4',
-'-I/usr/include/qt4/QtGui',
-'-I/usr/include/qt4/QtCore',
-'-I',
-'/home/jeremy/.vim/bundle/YouCompleteMe/autoload/../python/clang_includes'
-]
+flags += qtflags
