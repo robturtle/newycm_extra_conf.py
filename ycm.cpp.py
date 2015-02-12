3e2c427c331bb6f9078a18822c6bbee4c46f759f
LIU Yang <JeremyRobturtle@gmail.com>
2015-02-13 05:27:08 +0800

    add c++ path on OS X & tweak format

--- ycm.cpp.py
+++ ycm.cpp.py
@@ -35,50 +35,58 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',            # The Wmissing-braces is useless in c++11
-'-Wextra',
+  '-Wall'            # The Wmissing-braces is useless in c++11
+, '-Wextra'
 #'-Werror',          # Fucking unused parameter argc!!!
 #'-Wc++98-compat',   # Well i love c++11!!!
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-'-DUSE_CLANG_COMPLETER',
+, '-Wno-long-long'
+, '-Wno-variadic-macros'
+, '-fexceptions'
+, '-DNDEBUG'
+, '-DUSE_CLANG_COMPLETER'
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+, '-std=c++11'
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
+, '-x'
+, 'c++'
+, '-isystem'
+, '../BoostParts'
+, '-isystem'
 # This path will only work on OS X, but extra paths that don't exist are not
 # harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include'
+, '/System/Library/Frameworks/Python.framework/Headers'
+, '-isystem'
+, '../llvm/include'
+, '-isystem'
+, '../llvm/tools/clang/include'
+, '-I'
+, '.'
+, '-I'
+, './ClangCompleter'
+, '-isystem'
+, './tests/gmock/gtest'
+, '-isystem'
+, './tests/gmock/gtest/include'
+, '-isystem'
+, './tests/gmock'
+, '-isystem'
+, './tests/gmock/include'
+, '-I'
+, '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/c++/v1'
+, '-I'
+, '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1'
+, '-I'
+, '/usr/include'
+, '-I'
+, '/usr/local/include'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
